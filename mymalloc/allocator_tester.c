d8f73567ce305ca69b4435673da641f034eb6f0c
Shantanu Jain <jains@mit.edu>
2015-11-01 22:12:51 +0000

    WIP not running block joining. also added in a testing target. Run as make clean && make allocator_tester TEST=1 && ./allocator_tester

---
+++ mymalloc/allocator_tester.c
@@ -0,0 +1,18 @@
+#include "./allocator_interface.h"
+
+#ifdef TEST
+int main() {
+	my_init();
+	void* p1 = my_malloc(640);
+	void* p2 = my_malloc(640);
+
+	my_free(p1);
+	assert(big_free_list);
+	assert(big_free_list->size == 640);
+	
+	my_free(p2);
+	assert(big_free_list->next);
+	assert(big_free_list->next->size == 640);
+
+}
+#endif
